a2b28df74daa33ef4d5f3ed3e47366b0f69f9555
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-04-03 16:34:00 +0200

    changement slide

--- app/javascript/plugins/init_map.js
+++ app/javascript/plugins/init_map.js
@@ -1,13 +1,15 @@
-const initMap = () => {
+import mapboxgl from '!mapbox-gl';
 
-  mapboxgl.accessToken = '<your access token here>';
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-  });
-}
+// const initMap = () => {
+
+//   // mapboxgl.accessToken = '<your access token here>';
+//   const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+//   });
+// }
 
 //     // const map;
 
@@ -16,4 +18,52 @@ const initMap = () => {
 //       zoom: 8,
 //     });
 
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+    
+    new mapboxgl.Marker()
+    .setLngLat([ marker.lng, marker.lat ])
+    .setPopup(popup)
+    .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const initMap = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    mapboxgl.accessToken = ENV['MAPBOX_API_KEY'];
+
+    const map = new mapboxgl.Map({
+      container: mapElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v10', // style URL
+      // center: [2.3200410217200766, 48.8588897], // starting position [lng, lat]
+      // zoom: 15 // starting zoom
+    });
+    
+    const markers = JSON.parse(mapElement.dataset.markers);
+    // map.resize();
+    global.map = map;
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+  };
+
+};
+
 export { initMap };
+
+{/* <script>
+    mapboxgl.accessToken = "<%= ENV['MAPBOX_API_KEY'] %>";
+    const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [6.9010895, 46.5269159], // starting position [lng, lat]
+    zoom: 10 // starting zoom
+  });
+  </script> */}
--- app/javascript/plugins/init_scroll_to.js
+++ app/javascript/plugins/init_scroll_to.js
@@ -1,12 +1,14 @@
 const initScrollTo = () => {
   const scrollBtn = document.querySelector(".about-btn")
-
+  const scrollEnd = document.querySelector(".first-part")
+  // const scrollStart = document.getElementById("header");
   
   scrollBtn.addEventListener('click', () => {
-    window.scrollTo({
-      top: 900,
-      behavior: "smooth"
-    })
+    // window.scrollTo({
+    //   top: 900,
+    //   behavior: "smooth"
+    // })
+    scrollEnd.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   })
 
 
