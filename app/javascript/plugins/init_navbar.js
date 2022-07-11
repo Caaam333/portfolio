03246c013be2f3a795a9ea6e0930f613cad0b71b
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-07-11 16:23:38 +0200

    scroll

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -12,23 +12,40 @@
 
 
 const initNavbar = () => {
-  const navbar = document.getElementById("navbar")
-  const header = document.getElementById("header")
+  const navbar = document.querySelector("nav")
+  const header = document.querySelector("header")
+  const navH = 100
+  const winH = window.innerHeight
+  const perfH = winH - navH
 
     // navbar.addEventListener('', () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      // navbar.style.top = "0";
-      // navbar.style.background = "url("header.png")";
-      document.getElementById("header").style.height = "10vh";
+    // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //   // navbar.style.top = "0";
+    //   // navbar.style.background = "url("header.png")";
+    //   document.getElementById("header").style.height = "10vh";
+    //   console.log("navbar")
+    // } else {
+    //   // navbar.style.top = "-100px";
+    //   // navbar.style.background = "transparent";
+    //   document.getElementById("header").style.height = "100vh";
+    //   console.log(document.body.scrollTop)
+    // }
 
-      console.log("Camille")
-    } else {
-      // navbar.style.top = "-100px";
-      // navbar.style.background = "transparent";
-      document.getElementById("header").style.height = "100vh";
-    }
+    window.addEventListener('scroll', () => {
+
+      console.log(winH)
+      if (window.scrollY > (winH - navH)) {
+        header.classList.add("active");
+      } else {
+        header.classList.remove("active");
+      }
+    })
+
+
+    
 
   // })
+
 
 }
 
--- app/javascript/plugins/init_scroll_to.js
+++ app/javascript/plugins/init_scroll_to.js
@@ -1,25 +1,30 @@
 const initScrollTo = () => {
   const aboutBtn = document.querySelector(".about-btn")
+  const aboutBtnSvg = document.querySelector(".about-btn-svg")
   const aboutEnd = document.querySelector(".first-part")
   
   const cvBtn = document.querySelector(".cv-scroll")
   const cvEnd = document.querySelector(".cv")
 
+  const contactBtn = document.querySelector(".contact-btn")
+  const contactEnd = document.querySelector(".contact-list")
+
   aboutBtn.addEventListener('click', () => {
-    // window.scrollTo({
-    //   top: 900,
-    //   behavior: "smooth"
-    // })
+    aboutEnd.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"} + 100);
+  })
+
+  aboutBtnSvg.addEventListener('click', () => {
     aboutEnd.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   })
 
   cvBtn.addEventListener('click', () => {
-    // window.scrollTo({
-    //   top: 900,
-    //   behavior: "smooth"
-    // })
     cvEnd.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   })
+
+  contactBtn.addEventListener('click', () => {
+    contactEnd.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  })
+
 }
 
 export { initScrollTo };
