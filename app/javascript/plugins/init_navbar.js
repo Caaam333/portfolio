faa057cb554b056bd0c45700be943c734056a101
Camille LAGUERRE <camille.lgrr@gmail.com>
2022-07-16 15:17:53 +0200

    details

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -1,40 +1,56 @@
 
 
 const initNavbar = () => {
-  const navbar = document.querySelector("nav")
-  const header = document.querySelector("header")
-  const navH = 100
+  const navbar = document.querySelector("header")
+  const navH = navbar.getBoundingClientRect().height
+  // const header = document.querySelector("header")
+  const firstPart = document.querySelector(".first-part")
+  
+  const header2 = document.querySelector("#home-top-page")
+  const heaH = header2.getBoundingClientRect().height
   const winH = window.innerHeight
-  const perfH = winH - navH
+  const perfH = heaH - navH
 
-    // navbar.addEventListener('', () => {
-    // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    //   // navbar.style.top = "0";
-    //   // navbar.style.background = "url("header.png")";
-    //   document.getElementById("header").style.height = "10vh";
-    //   console.log("navbar")
-    // } else {
-    //   // navbar.style.top = "-100px";
-    //   // navbar.style.background = "transparent";
-    //   document.getElementById("header").style.height = "100vh";
-    //   console.log(document.body.scrollTop)
-    // }
+  
+  // console.log(perfH)
+  // console.log(navH)
+  // console.log(heaH)
+  // console.log(winH)
+  
+  window.addEventListener('scroll', () => {
 
-    window.addEventListener('scroll', () => {
+    if (window.scrollY > perfH) {
+      navbar.classList.add("active");
+    } else {
+      navbar.classList.remove("active");
+    }
+  })
 
-      if (window.scrollY > (winH - navH)) {
-        header.classList.add("active");
-      } else {
-        header.classList.remove("active");
-      }
-    })
+  // window.addEventListener('scroll', () => {
 
+  //   console.log(test)
 
-    
+  //   // if (firstPart.scrollY) {
+  //   //  header.classList.add("active");
+  //   // } else {
+  //   //  header.classList.remove("active");
+  //   // }
 
-  // })
-
-
-}
-
+  //  })   
+        
+  }
+      
+      // navbar.addEventListener('', () => {
+      // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      //   // navbar.style.top = "0";
+      //   // navbar.style.background = "url("header.png")";
+      //   document.getElementById("header").style.height = "10vh";
+      //   console.log("navbar")
+      // } else {
+      //   // navbar.style.top = "-100px";
+      //   // navbar.style.background = "transparent";
+      //   document.getElementById("header").style.height = "100vh";
+      //   console.log(document.body.scrollTop)
+      // }  
+      // })
 export { initNavbar };
